b1a413f694164de76cda9c4cf684919abc37104c
it21015144 <87602613+it21015144@users.noreply.github.com>
2021-09-13 10:26:01 +0000

    ex 1

--- Item.cpp
+++ Item.cpp
@@ -5,9 +5,22 @@ using namespace std;
 // 4. Implement Default Constructor Implementation
 // 5. Implement Overloaded Constructor Implementation
 // 6. Implement Destructor (display "Destructor Called")
+Item::Item()
+{
+  itemCode=0;
+  unitPrice=0;
+  discount=0;
+}
 
-void Item::setDiscount(float pdiscount) {
-  discount = pdiscount;
+Item::Item(int pitemCode,float punitPrice)
+{ 
+  itemCode=pitemCode;
+  unitPrice=punitPrice;
+}
+
+void Item::setDiscount(float pdiscount)
+{
+  discount=pdiscount;
 }
 
 float Item::getDiscount() {
@@ -18,7 +31,13 @@ float Item::discountedPrice() {
    return unitPrice - unitPrice * discount/100;
 }
 
-void Item::display() {
+void Item::display() 
+{
   cout << "Item : " << itemCode << endl;
   cout << "Discounted Price " << discountedPrice() << endl;
+}
+
+Item::~Item()
+{
+  cout << "destructor called" << endl;
 }
